15141c27f0ebacf46f25965ef5401a734861c615
funa <photiary@gmail.com>
2025-06-05 21:06:16 +0900

    google formatting 적용

--- src/main/java/com/example/SpringBootStarterKitApplication.java
+++ src/main/java/com/example/SpringBootStarterKitApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootStarterKitApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootStarterKitApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootStarterKitApplication.class, args);
+  }
 }
